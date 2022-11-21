f0c4ccf27240201de945a05a3b54e9e4a718d2fd
GuilhermeRatti <guirattimoraes@gmail.com>
2022-11-20 21:49:07 -0300

    Mais e mais e mais comentarios

--- Menu.h
+++ Menu.h
@@ -2,31 +2,42 @@
 #define _MENU_H_
 #include "Spotify.h"
 
-/*
+/************************************************************
 *    Funcao que lista as opcoes e executa cada uma delas
 *
 *    Retorna 0 (FINALIZAR) se foi escolido a opcao finalizar
 *    Retorna 1 (CONTINUAR) para qualquer outra opcao
-*/
+*************************************************************/
 int menu_show_options(p_Spotify spotify);
 
-/*
+
+/***************************************************************************************
 *    Funcao que envia o chamado de buscar musicas por titulo para a biblioteca spotify.h
 *
 *    Nao retorna nada
-*/
+****************************************************************************************/
 void menu_buscar_musica(p_Spotify spotify);
 
-/*
+
+/************************************************************************
 *    Funcao que envia o chamado de buscar uma musica por indice de vetor
 *
 *    Nao retorna nada
-*/
+*************************************************************************/
 void menu_lista_musica(p_Spotify spotify);
 
 
+/*****************************************************************
+*    Funcao que envia o chamado de criar uma playlist pro spotify
+*
+*    Nao retorna nada
+******************************************************************/
 void menu_playlist_cria(p_Spotify spotify);
 
+
+/*
+
+*/
 void menu_playlist_listar_todas(p_Spotify spotify);
 
 void menu_playlist_listar_uma(p_Spotify spotify);
--- Playlist.h
+++ Playlist.h
@@ -4,7 +4,7 @@
 
 typedef struct Playlist *p_Playlist;
 
-/*
+/***************************************************************************************
 *    Recebe como parametro o ponteiro de uma PLAYLIST e de uma MUSICA
 *
 *    Vai utilizar a funcao propriedade_media_playlist para calcular a media da playlist
@@ -12,7 +12,7 @@ typedef struct Playlist *p_Playlist;
 *    A partir da media da playlist vai calcular a DISTANCIA EUCLIDIANA
 *
 *    RETORNA A DISTANCIA EUCLIDIANA
-*/
+****************************************************************************************/
 double playlist_likeliness(p_Playlist playlist, p_Musica musica);
 
 p_Playlist playlist_cria();
